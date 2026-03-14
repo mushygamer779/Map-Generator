56debfefe11dddfae2b0526a3a09029b4040611a
mushygamer779 <akbarashurov2702@gmail.com>
2026-03-14 13:27:02 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,13 +21,25 @@ def handle_help(message):
 
 @bot.message_handler(commands=['show_city'])
 def handle_show_city(message):
-    city_name = message.text.split()[-1]
+    city_name = message.text.split()[-2]
+    color = message.text.split()[-1] if len(message.text.split()) > 2 else 'black'
+    print(city_name, color)
     # Реализуй отрисовку города по запросу
     user_id = message.chat.id
-    manager.create_graph(f"{user_id}.png", [city_name])
+    manager.create_graph(f"{user_id}.png", [city_name], color=color)
     with open(f"{user_id}.png", 'rb') as photo:
         bot.send_photo(user_id, photo)
 
+
+@bot.message_handler(commands=['draw_distance'])
+def handle_draw_distance(message):
+    cities = message.text.split()[1:]
+    if len(cities) == 2:
+        manager.draw_distance(cities[0], cities[1])
+        with open('distance.png', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, 'Пожалуйста, укажи ровно два города для сравнения!')
 
 
 @bot.message_handler(commands=['remember_city'])
@@ -49,6 +61,9 @@ def handle_show_visited_cities(message):
             bot.send_photo(message.chat.id, photo)
     else:
         bot.send_message(message.chat.id, 'У тебя нет сохраненных городов!')
+
+
+
 
 if __name__=="__main__":
     manager = DB_Map(DATABASE)
